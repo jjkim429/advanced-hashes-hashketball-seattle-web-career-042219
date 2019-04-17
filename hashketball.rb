c879cce9517583d5b2687a5434de09d5341473e6
Jennifer Kim <jjkim429@gmail.com>
2019-04-17 18:16:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,11 +129,12 @@ def game_hash
 end
 
 def num_points_scored(name)
+  answer = []
   game_hash.each do |team, details_hash|
     players_array = details_hash[:players]
       players_array.each do |player_details_hash|
         if player_details_hash == name
-          answer = player_details_hash[:points]
+          answer >> player_details_hash[:points]
         end
       end
   end
