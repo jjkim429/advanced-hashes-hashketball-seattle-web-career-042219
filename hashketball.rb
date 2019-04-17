c5b6d8920b9e1d133c5494c4607dc45085a740b4
Jennifer Kim <jjkim429@gmail.com>
2019-04-17 18:34:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,10 +130,14 @@ end
 
 def num_points_scored(name)
   hash = game_hash
-  hash.each do |location, info|
-    "#{location} and #{info}"
+  hash.each do |location, info| 
+    info.each do |attribute, stuff| 
+      if stuff.include?(name) 
+       return hash[location][attribute][name][:points]
+      end
+    end
+  end
 end
-
 
 def shoe_size(name)
   player = find_the_player(name)
